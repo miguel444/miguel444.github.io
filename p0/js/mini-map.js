37e34ef74d317bf3fbcab43b9cdd96dce670454a
miguel444 <miguel.a98@hotmail.es>
2019-11-06 20:38:46 +0100

    .

--- p0/js/mini-map.js
+++ p0/js/mini-map.js
@@ -25,7 +25,21 @@ class MiniMap{
 
     create_markers(renderer){
 
-        var court_lions = this.L.latLng([ 700, 560 ]);
+        var redIcon = new L.Icon({
+            iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+          });
+
+        if(renderer._current_node == diccionario_loc.get('Patio de los Leones')){
+            var court_lions = this.L.latLng([ 700, 560 ], {icon: redIcon});
+        }
+        else{
+            var court_lions = this.L.latLng([ 700, 560 ]);
+        }
         this.L.marker(court_lions).addTo(this.map).bindPopup("<b>PATIO DE LOS LEONES</b><br> Se encuentra aqui").openPopup().on('click', function(){
             moveToMarker('Patio de los Leones');
         });;
