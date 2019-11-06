eb250a91d8e7002ba97ac9e4dfca61751ca9a5d1
miguel444 <miguel.a98@hotmail.es>
2019-11-06 12:53:21 +0100

    Update mini-map.js

--- p0/js/mini-map.js
+++ p0/js/mini-map.js
@@ -18,9 +18,18 @@ document.getElementById("map-button").addEventListener('click', function(){
             // load a tile layer
     map.fitBounds(bounds);
 
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
     var court_lions = L.marker([ 700, 560 ],{
 
-        color: 'red'
+        icon: greenIcon
 
     }).addTo(map).bindPopup("<b>PATIO DE LOS LEONES</b><br> Se encuentra aqui").openPopup();
 
