93ef137f183271fe5ecf313e73081a7bbbff00a4
miguel444 <miguel.a98@hotmail.es>
2019-11-06 13:12:20 +0100

    Update mini-map.js

--- p0/js/mini-map.js
+++ p0/js/mini-map.js
@@ -46,7 +46,7 @@ document.getElementById("map-button").addEventListener('click', function(){
     var golden_room = L.latLng([ 760, 450 ]);
     L.marker(golden_room).addTo(map).bindPopup("<b>CUARTO DORADO</b><br>");
 
-    var travel = L.polyline([[arrayanes ], [ court_lions ]]).addTo(map);
+    var travel = L.polyline([[arrayanes.getlatLng() ], [ court_lions.getLatLng()]]).addTo(map);
     
     
 
