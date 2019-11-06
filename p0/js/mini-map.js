3b5bc48acf336ee1b17088980002ce826c82b258
miguel444 <miguel.a98@hotmail.es>
2019-11-06 13:29:47 +0100

    Update mini-map.js

--- p0/js/mini-map.js
+++ p0/js/mini-map.js
@@ -44,11 +44,9 @@ document.getElementById("map-button").addEventListener('click', function(){
     L.marker(comarabes).addTo(map).bindPopup("<b>SALA DE LOS ABENCERRAJES</b><br>");
     
     var golden_room = L.latLng([ 760, 450 ]);
-    L.marker(golden_room).addTo(map).bindPopup("<b>CUARTO DORADO</b><br>").on('click',function(){
-        alert(arrayanes.getLatLng())
-    });
+    L.marker(golden_room).addTo(map).bindPopup("<b>CUARTO DORADO</b><br>");
 
-    
+    var travel = L.polyline([ 720, 490 ],[ 750, 560 ]).addTo(map);
     
     
 
