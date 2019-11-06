041fd0dd4aaf8c9082e737accbdaf4c4a7848828
miguel444 <miguel.a98@hotmail.es>
2019-11-06 12:29:54 +0100

    Add files via upload

--- p0/js/mini-map.js
+++ p0/js/mini-map.js
@@ -1,44 +1,5 @@
 /* Mini Map */
 
-class mini_map{
-    constructor(inicial){
-        this._nodoIni = inicial;
-        this._posActual = inicial;
-        
-    }
-}
-    initialize_map(){
-
-        var map = L.map('map-preview', {
-            crs: L.CRS.Simple
-        });
-        
-        var bounds = [[0,0], [1000,1250]];
-        var image = L.imageOverlay('./assets/Alhambra.png', bounds).addTo(map);
-        
-                // load a tile layer
-                map.fitBounds(bounds);
-
-                var court_lions = L.latLng([ 700, 560 ]);
-          
-        L.marker(court_lions).addTo(map).bindPopup("<b>PATIO DE LOS LEONES</b><br> Se encuentra aqui").openPopup();
-    
-                var arrayanes = L.latLng([ 720, 490 ]);
-                L.marker(arrayanes).addTo(map).bindPopup("<b>ARRAYANES</b><br> hello");
-                
-        
-                var dos_hermanas = L.latLng([ 750, 560 ]);
-                L.marker(dos_hermanas).addTo(map).bindPopup("<b>DOS HERMANAS</b><br>");
-        
-                var comarabes = L.latLng([ 660, 550 ]);
-                L.marker(comarabes).addTo(map).bindPopup("<b>SALA DE LOS ABENCERRAJES</b><br>");
-        
-                var golden_room = L.latLng([ 760, 450 ]);
-                L.marker(golden_room).addTo(map).bindPopup("<b>CUARTO DORADO</b><br>");
-
-    }
-
-
 
   
 
@@ -46,10 +7,32 @@ class mini_map{
 document.getElementById("map-button").addEventListener('click', function(){
     popup_open("#map-preview");
 
+    var map = L.map('map-preview', {
+        crs: L.CRS.Simple
+    });
     
+    var bounds = [[0,0], [1000,1250]];
+    var image = L.imageOverlay('./assets/Alhambra.png', bounds).addTo(map);
+    
+            // load a tile layer
+            map.fitBounds(bounds);
 
-    initialize_map();
+            var court_lions = L.latLng([ 700, 560 ]);
+      
+    L.marker(court_lions).addTo(map).bindPopup("<b>PATIO DE LOS LEONES</b><br> Se encuentra aqui").openPopup();
 
+            var arrayanes = L.latLng([ 720, 490 ]);
+            L.marker(arrayanes).addTo(map).bindPopup("<b>ARRAYANES</b><br> hello");
+            
+    
+            var dos_hermanas = L.latLng([ 750, 560 ]);
+            L.marker(dos_hermanas).addTo(map).bindPopup("<b>DOS HERMANAS</b><br>");
+    
+            var comarabes = L.latLng([ 660, 550 ]);
+            L.marker(comarabes).addTo(map).bindPopup("<b>SALA DE LOS ABENCERRAJES</b><br>");
+    
+            var golden_room = L.latLng([ 760, 450 ]);
+            L.marker(golden_room).addTo(map).bindPopup("<b>CUARTO DORADO</b><br>");
     
     
 
